d872f5bfa039a0086dc51c1e0d7f403a7ee33f56
Jung Jaeyoung <93760723+jungjae0@users.noreply.github.com>
2022-12-01 01:06:24 +0900

    코드 설명 추가

--- app.py
+++ app.py
@@ -1,43 +1,50 @@
-import atexit
-
 import pandas as pd
 import serial
 import time
 import atexit
 
-# https://kocoafab.cc/fboard/view/2159
-
+# 아두이노 포트 번호 입력(아두이노와 Python의 연결)
 s = serial.Serial('COM3', 9600)
 
 # csv 파일 정리
 def convert_data(filename, name):
 
+    # csv파일 읽기 및 인코딩
     df = pd.read_csv(filename, encoding='cp949')
+    # column명 재설정
     df.rename(columns=df.iloc[1], inplace=True)
+    # 필요없는 행 제거
     df.drop([0,1], inplace=True)
+    # 정리한 파일 저장
     df.to_csv(f'./data/{name}.csv', encoding='cp949')
+    # 정리한 파일 읽기
     df = pd.read_csv(f'./data/{name}.csv', encoding='cp949')
 
     return df
 
 
-# 아두이노 우노에 신호보내기
+# 아두이노 우노에 신호보내기위한 함수 정의
 def send_signal_to_sfarm(msg):
     if (s.readable()):
         s.write("{}\n".format(msg).encode())
     else:
         print("message is not transferred")
 
-# 환경제어_환경장치
+#### 환경제어_환경장치 ####
 # vent - 40, 50, 60, 70, 80, 90
 def cvent(df_control):
+    # df에서 Vent열만 읽기
     vent_value = df_control["Vent"]
+    # Vent 한 셀의 값이 40이상 60미만이면 "CFAN-1"
     if 40 <= vent_value < 60:
         return "CFAN-1"
+    # Vent 한 셀의 값이 60이상 80미만이면 "CFAN-1"
     elif 60 <= vent_value <80:
         return "CFAN-2"
+    # Vent 한 셀의 값이 80이상이면 "CFAN-1"
     elif 80 <= vent_value:
         return "CFAN-3"
+    # Vent 한 셀의 값이 30이하이면 "CFAN-0"
     else:
         return "CFAN-0"
 
@@ -45,18 +52,15 @@ def cvent(df_control):
 def cheat(df_control):
     heat_value = df_control["Heater"]
     if heat_value == 3:
-        # send_signal_to_sfarm("HEAT-1")
         return "CHEAT-3"
     elif heat_value == 2:
-        # send_signal_to_sfarm("HEAT-1")
         return "CHEAT-2"
     elif heat_value == 1:
-        # send_signal_to_sfarm("HEAT-1")
         return "CHEAT-1"
     else:
         return "CHEAT-0"
 
-# fog-1은 on, 2는 off
+# fog - 1은 on, 0은 off
 def cfog(df_control):
     fog_value = df_control["Fogging"]
     if fog_value == 0:
@@ -65,7 +69,7 @@ def cfog(df_control):
         return "CFOG-0"
 
 
-# 미설치_환경장치
+#### 미설치_환경장치 ####
 # vent - 40, 50, 60, 70, 80, 90
 def nvent(df_normal):
     vent_value = df_normal["Vent"]
@@ -77,37 +81,15 @@ def nvent(df_normal):
         return "CFAN-3"
     else:
         return "CFAN-0"
-    # if vent_value == 90:
-    #     # send_signal_to_sfarm("FAN-1")
-    #     return "NFAN-9"
-    # elif vent_value == 80:
-    #     # send_signal_to_sfarm("FAN-1")
-    #     return "NFAN-8"
-    # elif vent_value == 70:
-    #     # send_signal_to_sfarm("FAN-1")
-    #     return "NFAN-7"
-    # elif vent_value == 60:
-    #     # send_signal_to_sfarm("FAN-1")
-    #     return "NFAN-6"
-    # elif vent_value == 50:
-    #     # send_signal_to_sfarm("FAN-1")
-    #     return "NFAN-5"
-    # else:
-    #     return "NFAN-0"
-    #     # send_signal_to_sfarm("FAN-0")
-
 
 # heat - 3, 2, 1, 0
 def nheat(df_normal):
     heat_value = df_normal["Heater"]
     if heat_value == 3:
-        # send_signal_to_sfarm("HEAT-1")
         return "NHEAT-3"
     elif heat_value == 2:
-        # send_signal_to_sfarm("HEAT-1")
         return "NHEAT-2"
     elif heat_value == 1:
-        # send_signal_to_sfarm("HEAT-1")
         return "NHEAT-1"
     else:
         return "NHEAT-0"
@@ -121,41 +103,51 @@ def nfog(df_normal):
     else:
         return "NFOG-0"
 
+# python 프로그램 실행을 중단하였을 때 아두이노 기기들이 모두 작동하지 않도록 하기 위한 함수 정의
+# python 프로그램이 중단되었을 때 모두 끄는 명렁어를 아두이노에 전달한다.
 def handle_exit():
     return send_signal_to_sfarm("CFAN-0CHEAT-0CFOG-0NFAN-0NHEAT-0NFOG-0")
 
 def main():
 
+    # 제공된 시나리오 더미 데이터 정의
     control_filenmae = "./data/시나리오 더미 데이터_환경제어.csv"
     normal_filename = "./data/시나리오 더미 데이터_미설치.csv"
 
+    # 앞의 더미 데이터를 정리 후 저장할 파일 정의
     df_control_name = "control_scenario"
     df_normal_name = "normal_scenario"
 
+    # 앞서 정의한 함수를 실행 -> 제공된 시나리오 더미 데이터를 정리, 정리한 데이터를 불러옴
     df_control = convert_data(control_filenmae, df_control_name)
     df_normal = convert_data(normal_filename, df_normal_name)
 
+    # 각 행에 대한 연산을 반복
     for i in range(len(df_control.index)):
         control_scenario = df_control.iloc[i]
         normal_scenario = df_normal.iloc[i]
 
-        # print(df_control.iloc[i], df_normal.iloc[i])
+        # 각 셀에 해당하는 조건에 맞는 return 값을 모아 list형태로 저장
         control_rule = [cvent(control_scenario), cheat(control_scenario), cfog(control_scenario)]
         normal_rule = [nvent(normal_scenario), nheat(normal_scenario), nfog(normal_scenario)]
 
-        # 아두이노에 시나리오 신호 보내기
+        # 앞선 list 형태를 문자열 형태로 변경하여 아두이노에 신호전달
+        # 앞서 정의한 아두이노에 신호를 보내기 위한 함수 실행
         send_signal_to_sfarm("".join(control_rule))
         send_signal_to_sfarm("".join(normal_rule))
 
         print(f"send signal: {''.join(control_rule)}")
         print(f"send signal: {''.join(normal_rule)}")
+
+        # 한 행을 몇 초 동안 수행할 것인지 정함
+        # '3' = '3초' ; 원하는 시간으로 바꾸면 됨
         time.sleep(3)
 
+        # 프로그램을 중단하였을 떄 led 및 환기팬 작동을 멈춤
         atexit.register(handle_exit)
 
     # print(df_control["Heater"].unique())
     # print(df_normal["Heater"].unique())
-
 
 
 if __name__ == '__main__':
